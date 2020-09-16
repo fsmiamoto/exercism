3990e3a57a80ebbaa43c6fd4f06c141c03c2d138
Francisco Miamoto <fsmiamoto@gmail.com>
2020-09-16 09:28:28 -0300

    elixir: replace if with cond

--- elixir/markdown/lib/markdown.ex
+++ elixir/markdown/lib/markdown.ex
@@ -20,14 +20,10 @@ defmodule Markdown do
   end
 
   defp process(t) do
-    if String.starts_with?(t, "#") || String.starts_with?(t, "*") do
-      if String.starts_with?(t, "#") do
-        enclose_with_header_tag(parse_header_md_level(t))
-      else
-        parse_list_md_level(t)
-      end
-    else
-      enclose_with_paragraph_tag(String.split(t))
+    cond do
+      String.starts_with?(t, "#") -> enclose_with_header_tag(parse_header_md_level(t))
+      String.starts_with?(t, "*") -> parse_list_md_level(t)
+      true -> enclose_with_paragraph_tag(String.split(t))
     end
   end
 
