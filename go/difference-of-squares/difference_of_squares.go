5f619f8ff0895b42897c78d63f47b9f37cce5874
Francisco Miamoto <fsmiamoto@gmail.com>
2020-12-26 12:07:16 -0300

    go: add descriptive comments to exported functions

--- go/difference-of-squares/difference_of_squares.go
+++ go/difference-of-squares/difference_of_squares.go
@@ -1,13 +1,17 @@
 package diffsquares
 
+// SumOfSquares calculates the sum of the square of the first n positive integers - 1,2,3...
 func SumOfSquares(n int) int {
 	return n * (n + 1) * (2*n + 1) / 6
 }
 
+// SquareOfSum calculates the square of the sum of the first n positive integers - 1,2,3...
 func SquareOfSum(n int) int {
 	return sq(n) * sq(n+1) / 4
 }
 
+// Difference calculates the difference between the square of the sum and the
+// sum of the square of the first n positive integers - 1,2,3..
 func Difference(n int) int {
 	return SquareOfSum(n) - SumOfSquares(n)
 }
