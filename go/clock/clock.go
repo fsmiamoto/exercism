0f728fbc28eead31e3504f5fc152def3bd0fc609
Francisco Miamoto <fsmiamoto@gmail.com>
2021-02-26 22:26:51 -0300

    go: refactor clock to not store hours

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -4,37 +4,29 @@ import "fmt"
 
 // Clock represents a clock
 type Clock struct {
-	hours   int
 	minutes int
 }
 
 // New creates a new Clock with the provided hours and minutes
 func New(h, m int) Clock {
-	minutes := m % 60
-	hours := (h + m/60) % 24
-
+	minutes := m + h*60
+	minutes %= 24 * 60
 	if minutes < 0 {
-		minutes += 60
-		hours--
+		minutes += 24 * 60
 	}
-
-	if hours < 0 {
-		hours += 24
-	}
-
-	return Clock{hours, minutes}
+	return Clock{minutes}
 }
 
 func (c Clock) String() string {
-	return fmt.Sprintf("%02d:%02d", c.hours, c.minutes)
+	return fmt.Sprintf("%02d:%02d", c.minutes/60, c.minutes%60)
 }
 
 // Subtract subtracts an ammount of minutes from the Clock, returning a new Clock
 func (c Clock) Subtract(minutes int) Clock {
-	return New(c.hours, c.minutes-minutes)
+	return New(c.minutes/60, (c.minutes%60)-minutes)
 }
 
 // Add adds an ammount of minutes from the Clock, returning a new Clock
 func (c Clock) Add(minutes int) Clock {
-	return New(c.hours, c.minutes+minutes)
+	return New(c.minutes/60, (c.minutes%60)+minutes)
 }
