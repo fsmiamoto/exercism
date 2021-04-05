aa7835166a5805629f2014ea184fbb779fc40563
Francisco Miamoto <fsmiamoto@gmail.com>
2021-04-04 22:27:02 -0300

    haskell: update space-age

--- haskell/space-age/src/SpaceAge.hs
+++ haskell/space-age/src/SpaceAge.hs
@@ -10,11 +10,20 @@ data Planet = Mercury
             | Neptune
 
 ageOn :: Planet -> Float -> Float
-ageOn Earth seconds   = seconds / 31557600
-ageOn Mercury seconds = ageOn Earth seconds / 0.2408467
-ageOn Venus seconds   = ageOn Earth seconds / 0.61519726
-ageOn Mars seconds    = ageOn Earth seconds / 1.8808158
-ageOn Jupiter seconds = ageOn Earth seconds / 11.862615
-ageOn Saturn seconds  = ageOn Earth seconds / 29.447498
-ageOn Uranus seconds  = ageOn Earth seconds / 84.016846
-ageOn Neptune seconds = ageOn Earth seconds / 164.79132
+ageOn planet secs = secs / periodInSecsFor planet
+
+periodInSecsFor :: Planet -> Float
+periodInSecsFor planet = earthPeriodMultiplierFor planet * earthPeriodInSecs
+
+earthPeriodInSecs :: Num a => a
+earthPeriodInSecs = 31557600
+
+earthPeriodMultiplierFor :: Planet -> Float
+earthPeriodMultiplierFor Mercury =  0.2408467 
+earthPeriodMultiplierFor Venus   =  0.61519726
+earthPeriodMultiplierFor Earth   =  1.0       
+earthPeriodMultiplierFor Mars    =  1.8808158 
+earthPeriodMultiplierFor Jupiter =  11.862615 
+earthPeriodMultiplierFor Saturn  =  29.447498 
+earthPeriodMultiplierFor Uranus  =  84.016846 
+earthPeriodMultiplierFor Neptune =  164.79132 
