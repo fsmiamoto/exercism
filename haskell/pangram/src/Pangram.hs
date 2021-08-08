b335e7757287a0b96dfb383ef349bc39eec19ff3
Francisco Miamoto <francisco@dorayaki.co>
2021-08-08 19:00:01 -0300

    haskell: improve pangram

--- haskell/pangram/src/Pangram.hs
+++ haskell/pangram/src/Pangram.hs
@@ -1,25 +1,11 @@
 module Pangram (isPangram) where
 
-import Data.Char (isLetter, toLower)
-
-letters :: String
-letters = ['a'..'z']
+import Data.Char (toLower, isAsciiLower)
+import Data.List (sort, group)
 
 isPangram :: String -> Bool
-isPangram ""  = False
-isPangram text = length filtered == 0 
-    where filtered = filterBy letters (normalize text)
-
-filterBy :: String -> String -> String
-filterBy ls [] = ls
-filterBy ls (m:ms) = filterBy (filter (\l -> l /= m) ls) ms
-
-removeNonLetters :: String -> String
-removeNonLetters = filter isLetter
-
-toLowerCase :: String -> String
-toLowerCase = map toLower
+isPangram text = l == 26 where l = length $ (group . sort) $ normalize text
 
 normalize :: String -> String
-normalize = removeNonLetters . toLowerCase
+normalize = filter isAsciiLower . map toLower
 
