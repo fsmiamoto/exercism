b0e3ebd174f1380768cdb79983ad5e64bc1fbe5f
Francisco Miamoto <francisco@dorayaki.co>
2021-10-10 12:12:15 -0300

    rust: add semi-structure-logs

---
+++ rust/semi-structured-logs/src/lib.rs
@@ -0,0 +1,33 @@
+// This stub file contains items which aren't used yet; feel free to remove this module attribute
+// to enable stricter warnings.
+#![allow(unused)]
+
+/// various log levels
+#[derive(Clone, PartialEq, Debug)]
+pub enum LogLevel {
+    Debug,
+    Info,
+    Warning,
+    Error,
+}
+/// primary function for emitting logs
+pub fn log(level: LogLevel, message: &str) -> String {
+    match level {
+        LogLevel::Debug => debug(message),
+        LogLevel::Info => info(message),
+        LogLevel::Warning => warn(message),
+        LogLevel::Error => error(message),
+    }
+}
+pub fn debug(message: &str) -> String {
+    String::from("[DEBUG]: ") + message
+}
+pub fn info(message: &str) -> String {
+    String::from("[INFO]: ") + message
+}
+pub fn warn(message: &str) -> String {
+    String::from("[WARNING]: ") + message
+}
+pub fn error(message: &str) -> String {
+    String::from("[ERROR]: ") + message
+}
