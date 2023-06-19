d7612efe1dafae5dd44e8d0035e7bc5a771fc17a
Flavio Miamoto <hello@dorayaki.co>
2023-06-18 21:35:11 -0300

    rust: improve clock

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -6,11 +6,9 @@ pub struct Clock {
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        let trimmed_minutes = minutes.rem_euclid(60) as i8;
-        let extra_hours = ((minutes as f64) / 60.0).floor() as i32;
         Clock {
-            hours: (hours + extra_hours).rem_euclid(24) as i8,
-            minutes: trimmed_minutes,
+            hours: (hours + minutes.div_euclid(60)).rem_euclid(24) as i8,
+            minutes: minutes.rem_euclid(60) as i8,
         }
     }
 
@@ -19,6 +17,6 @@ impl Clock {
     }
 
     pub fn to_string(&self) -> String {
-        format!("{:0>2}:{:0>2}", self.hours, self.minutes)
+        format!("{:02}:{:02}", self.hours, self.minutes)
     }
 }
